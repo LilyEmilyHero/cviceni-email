d4c60315f97003ab17a57b2054e03f3d598dbd40
Linda Hrdinová <hrlin@seznam.cz>
2023-09-25 18:47:01 +0200

    zpracování cvičení

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+/*Každý e-mail je třeba zakončit zdvořilým pozdravem.
+
+V souboru index.js vytvořte funkci bez parametrů s názvem goodbye. Tato funkce vloží do odstavce s třídou email__closing rozloučení „Na shledanou“.
+
+Zavolejte funkci goodbye() na konci souboru index.js. Ověřte, že se na zobrazené stránce správně změnil pozdrav na konci e-mailu.
+
+Končit e-mail jen slovy „Na shledanou“ je nezdvořilé. Přidejte proto do funkce goodbye parametr představující jméno pisatele e-mailu. Funkce do posledního odstavce vloží koncový pozdrav i se jménem. Příklad použití:
+
+goodbye('Pavel Ovesný');
+Ve stránce bude
+
+S pozdravem Pavel Ovesný*/
+
 console.log('funguju');
+
+const goodbye = (jmeno) => {
+  const closingElement = document.querySelector('.email__closing');
+  closingElement.innerHTML = `Na shledanou ${jmeno}`;
+};
+
+goodbye('Pavel Oveslý');
